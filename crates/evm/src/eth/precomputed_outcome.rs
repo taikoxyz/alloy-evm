72d482c79c517ee7b4377bc4546ddae16a48df64
Brecht Devos <Brechtp.Devos@gmail.com>
2026-01-16 18:56:13 +0900

    redesign: 38.3 canonical compare tests

--- crates/evm/src/eth/precomputed_outcome.rs
+++ crates/evm/src/eth/precomputed_outcome.rs
@@ -631,4 +631,235 @@ mod tests {
 
         assert_eq!(err, PrecomputedOutcomeValidationError::ExtraDataMismatch { expected_len: 2, got_len: 2 });
     }
+
+    #[test]
+    fn canonicalize_rejects_state_root_mismatch() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 5;
+        header.state_root = B256::from([0x11; 32]);
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: None,
+            blob_gas_used: None,
+            requests_hash: None,
+        };
+
+        let err = canonicalize_and_compare(
+            &header,
+            &inputs,
+            B256::from([0x22; 32]),
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .unwrap_err();
+
+        assert!(matches!(err, PrecomputedOutcomeValidationError::StateRootMismatch { .. }));
+    }
+
+    #[test]
+    fn canonicalize_rejects_receipts_root_mismatch_for_empty_receipts_form() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 5;
+        header.state_root = B256::from([0x11; 32]);
+        header.receipts_root = B256::from([0x22; 32]);
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: B256::from([0x33; 32]),
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: None,
+            blob_gas_used: None,
+            requests_hash: None,
+        };
+
+        let err = canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .unwrap_err();
+
+        assert!(matches!(err, PrecomputedOutcomeValidationError::ReceiptsRootMismatch { .. }));
+    }
+
+    #[test]
+    fn canonicalize_rejects_blob_gas_used_when_cancun_inactive() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 5;
+        header.state_root = B256::from([0x11; 32]);
+        header.blob_gas_used = Some(0);
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: None,
+            blob_gas_used: None,
+            requests_hash: None,
+        };
+
+        let err = canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .unwrap_err();
+
+        assert_eq!(err, PrecomputedOutcomeValidationError::BlobGasUsedUnexpected);
+    }
+
+    #[test]
+    fn canonicalize_rejects_excess_blob_gas_when_cancun_inactive() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 5;
+        header.number = 42;
+        header.state_root = B256::from([0x11; 32]);
+        header.excess_blob_gas = Some(0);
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: None,
+            blob_gas_used: None,
+            requests_hash: None,
+        };
+
+        let err = canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .unwrap_err();
+
+        assert_eq!(err, PrecomputedOutcomeValidationError::ExcessBlobGasUnexpected);
+    }
+
+    #[test]
+    fn canonicalize_rejects_requests_hash_when_prague_inactive() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 0;
+        header.state_root = B256::from([0x11; 32]);
+        header.requests_hash = Some(B256::from([0x22; 32]));
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: None,
+            blob_gas_used: None,
+            requests_hash: header.requests_hash,
+        };
+
+        let err = canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .unwrap_err();
+
+        assert_eq!(err, PrecomputedOutcomeValidationError::RequestsHashUnexpected);
+    }
+
+    #[test]
+    fn canonicalize_accepts_withdrawals_root_when_shanghai_active() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 15;
+        header.state_root = B256::from([0x11; 32]);
+        header.withdrawals_root = Some(B256::from([0x22; 32]));
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: header.withdrawals_root,
+            blob_gas_used: None,
+            requests_hash: None,
+        };
+
+        canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 0, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .expect("withdrawals_root is required and compared once shanghai is active");
+    }
+
+    #[test]
+    fn canonicalize_accepts_requests_hash_when_prague_active() {
+        let schedule = TestSchedule;
+        let mut header = test_header();
+        header.timestamp = 35;
+        header.number = 42;
+        header.state_root = B256::from([0x11; 32]);
+        header.withdrawals_root = Some(B256::from([0x22; 32]));
+        header.blob_gas_used = Some(0);
+        header.excess_blob_gas = Some(schedule.blob_params_at(header.number, header.timestamp).next_block_excess_blob_gas_osaka(0, 0, 0));
+        header.requests_hash = Some(B256::from([0x33; 32]));
+
+        let inputs = ComparisonInputs {
+            tx_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            gas_used: header.gas_used,
+            withdrawals_root: header.withdrawals_root,
+            blob_gas_used: header.blob_gas_used,
+            requests_hash: header.requests_hash,
+        };
+
+        canonicalize_and_compare(
+            &header,
+            &inputs,
+            header.state_root,
+            header.number,
+            header.timestamp,
+            header.extra_data.as_ref(),
+            ParentHeaderView { timestamp: 15, blob_gas_used: None, excess_blob_gas: None, base_fee_per_gas: 0 },
+            &schedule,
+        )
+        .expect("requests_hash is required and compared once prague is active");
+    }
 }
